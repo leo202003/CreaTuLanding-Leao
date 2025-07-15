83a816ad5e8a116ae924c0f28d4b91b07b0b170e
leonardo.leao <leonardo.leao90@gmail.com>
2025-07-15 16:07:55 -0300

    import NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { NavBar } from "./components/NavBar";
 import { ItemListContainer } from "./components/ItemListContainer";
 import { ItemDetailContainer } from "./components/ItemDetailContainer";
-import { NotFound } from "./components/NotFound";
+import { NotFound } from "./components/notFound";
 
 export function App() {
   return (
